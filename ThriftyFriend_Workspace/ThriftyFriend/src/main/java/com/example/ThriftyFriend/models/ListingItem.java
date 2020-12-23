8c5c35dcc53e7b8ac1dc0e8cd68f2190cd4e5d45
Kelly Howell <worldwarwe_5@hotmail.com>
2020-12-22 18:56:28 -0600

    removed extra imports from listingItem model

--- ThriftyFriend_Workspace/ThriftyFriend/src/main/java/com/example/ThriftyFriend/models/ListingItem.java
+++ ThriftyFriend_Workspace/ThriftyFriend/src/main/java/com/example/ThriftyFriend/models/ListingItem.java
@@ -1,11 +1,6 @@
 package com.example.ThriftyFriend.models;
 
-import java.util.List;
 
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 
 public class ListingItem 
 {
