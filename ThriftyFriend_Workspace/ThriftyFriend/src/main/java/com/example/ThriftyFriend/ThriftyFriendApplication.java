c82544647827da64fa7cebd250ecaadf4b4ff085
Dasoop <ssummer.lance@gmail.com>
2021-11-14 17:39:30 -0800

    add dockerfile and docker-compose

--- ThriftyFriend_Workspace/ThriftyFriend/src/main/java/com/example/ThriftyFriend/ThriftyFriendApplication.java
+++ ThriftyFriend_Workspace/ThriftyFriend/src/main/java/com/example/ThriftyFriend/ThriftyFriendApplication.java
@@ -47,16 +47,16 @@ public class ThriftyFriendApplication
 		}
 	}
 	
-    @Bean
-    public TomcatServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        Connector ajpConnector = new Connector("AJP/1.3");
-        ajpConnector.setPort(9090);
-        ajpConnector.setSecure(false);
-        ajpConnector.setAllowTrace(false);
-        ajpConnector.setScheme("http");
-       ((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
-    tomcat.addAdditionalTomcatConnectors(ajpConnector);
-    return tomcat;
-    }
+//    @Bean
+//    public TomcatServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        Connector ajpConnector = new Connector("AJP/1.3");
+//        ajpConnector.setPort(9090);
+//        ajpConnector.setSecure(false);
+//        ajpConnector.setAllowTrace(false);
+//        ajpConnector.setScheme("http");
+//       ((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
+//    tomcat.addAdditionalTomcatConnectors(ajpConnector);
+//    return tomcat;
+//    }
 }
